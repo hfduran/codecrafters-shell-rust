236a6b34f0e8497a98f72dffa6ba059e68d9d050
Henrique Duran <henriqueduran15@gmail.com>
2025-12-24 20:06:08 -0300

    command not found

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
+    print_prompt();
+}
+
+fn print_prompt() {
     print!("$ ");
     io::stdout().flush().unwrap();
+    read_input();
+}
+
+fn read_input() {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let input = input.trim();
+    print_command_not_found(&input);
+}
+
+fn print_command_not_found(command: &str) {
+    print!("{}: command not found", command)
 }
