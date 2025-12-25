c8cfd9e84a451fc5ab6b219bd7d27f8e981d18b9
Henrique Duran <henriqueduran15@gmail.com>
2025-12-25 11:04:16 -0300

    exit command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::io::{self, Write};
+use std::{
+    io::{self, Write},
+    process,
+};
 
 fn main() {
     repl()
@@ -8,7 +11,7 @@ fn repl() {
     loop {
         print_prompt();
         let input = read_input();
-        print_command_not_found(&input);
+        evaluate_command(&input);
     }
 }
 
@@ -25,6 +28,17 @@ fn read_input() -> String {
     input.trim().to_string()
 }
 
+fn evaluate_command(command: &str) {
+    match command {
+        "exit" => execute_exit_command(),
+        _ => print_command_not_found(command),
+    }
+}
+
 fn print_command_not_found(command: &str) {
     println!("{}: command not found", command)
+}
+
+fn execute_exit_command() {
+    process::exit(0);
 }
